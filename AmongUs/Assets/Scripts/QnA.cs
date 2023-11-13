a0bb09968d14d1f696b186e5c88f425a5dda8e72
MikeTheBuddy <michael.krivobarski@gmail.com>
2023-11-13 16:06:53 -0600

    Added ReadFile Stuff
    Adding Readfile stuff into QnA file

--- AmongUs/Assets/Scripts/QnA.cs
+++ AmongUs/Assets/Scripts/QnA.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,9 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Linq;
 using TMPro;
-
+using System.IO;
+using Random = UnityEngine.Random;
+//using QuizletBackend;
 
 public class QnA : MonoBehaviour
 {
@@ -39,9 +42,16 @@ public class QnA : MonoBehaviour
         Q3.answer = "2";
         Q4.question = "1+0=?";
         Q4.answer = "1";
-        //Question QArray = {Q1, Q2, Q3, Q4};
         List<Question> temp = new List<Question> ();
-        List<Question> QAlist = new List<Question> {Q1, Q2, Q3, Q4};
+        List<Question> QAlist = ReadQuizletFile("Bias\tArises when the design of a study is very likely to underestimate or overestimate the value you want to know\r\nCluster Sample\tA sampling method in which you first divide the population into smaller groups that mirror the characteristics of the population and random groups are chosen\r\nConfounding\tThis occurs when two variables are associated in such a way that their effects on a response variable cannot be distinguished from each other\r\nControl Group\tA group that receives no treatment or a baseline treatment\r\nControl, Random Assignment, Replication\tName the three principles of experimental design\r\nConvenience Sample\tChoosing individuals who are easiest to reach to collect results\r\nExperimental Study\tA study that deliberately imposes some treatment on individuals to measure their responses\r\nExperimental Units\tThe collection of individuals to which treatments are applied\r\nLurking Variable\tA variable that is not being measured but could affect the response in an experiment\r\nNonresponse\tWhen an individual chosen for the sample can't be contacted or refuses to participate in the sample\r\nObservational Study\tA study that observes individuals and measures variables of interest\r\nPopulation\tThe entire group of individuals about which we want information\r\nRandom Assignment\tA study in which the treatments are assigned to all the experimental units completely by chance\r\nResponse Bias\tA systematic pattern of incorrect responses in a sample survey\r\nSample\tThe part of the population from which we actually collect information\r\nSimple Random Sample\tA sampling method in which individuals from the population are chosen in such a way that every set of individuals has an equal chance to be the sample actually selected\r\nStratified Sample\tA sampling method in which the population is first classified into groups of similar individuals and individuals from each group are chosen\r\nSystematic Sample\tA sampling method in which each nth person is chosen to participate in the survey\r\nTreatment\tA specific condition applied to the individuals in an experiment\r\nUndercoverage\tWhen some groups in the population are left out of the process of choosing the sample\r\nVoluntary Response Sample\tA sampling method that consists of people who choose to responding to a general appeal survey");
+        QAlist.Add(Q1);
+        QAlist.Add(Q2);
+        QAlist.Add(Q3);
+        QAlist.Add(Q4);
+        /*for(int i = 0; i < QAlist.Count;i++)
+        {
+            //Debug.Log(QAlist[i].question);
+        }*/
         Question selcetedQ = selectQuestion(QAlist, ref temp);
         questionText.text = selcetedQ.question;
         //Debug.Log(selcetedQ.question);
@@ -126,7 +136,7 @@ public class QnA : MonoBehaviour
     }
 
     public Question selectQuestion(List<Question> questionList, ref List<Question> temp) {
-        int QAindex = Random.Range(0,4);
+        int QAindex = Random.Range(0,questionList.Count);
         Question TQ = questionList[QAindex];
         questionList.RemoveAt(QAindex);
         int loops = 0;
@@ -139,5 +149,41 @@ public class QnA : MonoBehaviour
         return TQ;
     }
 
+    public List<Question> ReadQuizletFile(string filename)
+    {
+        List<Question> temp = new();
 
+        try
+        {
+            // Pass the file path and file name to the StreamReader constructor
+            // This needs to be corrected to an appropriate directory for the project
+            //using StreamReader sr = new(filename);
+            StringReader quiztxt = new(filename);
+            // Read the first line of text
+            //String line = sr.ReadLine();
+            String line = quiztxt.ReadLine();
+            // Continue to read until you reach end of file
+            while (line != null)
+            {
+                String[] tempLine = line.Split('\t');
+
+                // templine[0] holds the string of the question
+                // templine[1] holds the string of the answer
+                temp.Add(new Question { question = tempLine[0], answer = tempLine[1]});
+                // Read the next line
+                // line = sr.ReadLine();
+                line = quiztxt.ReadLine();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Executing finally block.");
+        }
+
+        return temp;
+    }
 }
